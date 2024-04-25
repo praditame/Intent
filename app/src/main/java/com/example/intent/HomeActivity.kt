5f817c5fd4eb48e369a140b6c18865092a23da72
praditame <praditame@gmail.com>
2024-04-25 11:02:02 +0700

    add intent explicit

--- app/src/main/java/com/example/intent/HomeActivity.kt
+++ app/src/main/java/com/example/intent/HomeActivity.kt
@@ -1,12 +1,19 @@
 package com.example.intent
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), View.OnClickListener{
+
+    lateinit var btnExplicit: Button
+    lateinit var btnImplicit: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +22,30 @@ class HomeActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        btnExplicit = findViewById(R.id.btnExplicit)
+        btnImplicit = findViewById(R.id.btnImplicit)
+
+        btnExplicit.setOnClickListener(this)
+        btnImplicit.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btnExplicit -> {
+                //fungsi intent dijalankan (di dlm aksi button)
+                //deklarasi intent
+                val intentExplicit = Intent(this@HomeActivity,
+                    PageActivity2::class.java)
+
+                //run intent
+                startActivity(intentExplicit)
+            }
+
+            R.id.btnImplicit -> {
+
+            }
         }
     }
 }
